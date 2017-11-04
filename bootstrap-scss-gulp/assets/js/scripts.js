afb988059ca3d7b5a2bb3f7264a3ef8d2e0e60c5
Dallas Gale <dallasgale@hotmail.com>
2017-11-04 14:42:21 +1100

    jQuery functonality
    Added onClicks and promts for data

--- bootstrap-scss-gulp/assets/js/scripts.js
+++ bootstrap-scss-gulp/assets/js/scripts.js
@@ -8,5 +8,22 @@ $(function(){
         'backgroundColor': '#f74902', 'color': 'white'
     });
 
-    
+    var output = $('#consoleWindow');
+
+
+
+    var place = $('.place').click(function() {
+        var xy = prompt('Take your robot for a walk! \n Example: \"0,3 NORTH\"');
+
+        if (xy != null) {
+            output.append('<code> PLACE: ' + xy + '</code><br />');
+        }
+    });
+
+
+    var consoleWindow = $('.move').click(function() {
+        var title = $(this).attr('title')
+        console.log(title);
+        output.append('<code>' + title + '</code><br />');
+    });
 });
