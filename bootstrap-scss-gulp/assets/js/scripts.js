364b9c91f684d90eb453a68f43d25292a311a3af
Dallas Gale <dallasgale@hotmail.com>
2017-11-04 18:32:48 +1100

    Variables & Functions added
    * Position
    * - + integers for up down or left right direction
    * if robot value is hight than 0 but lower than 5
    * Report data

--- bootstrap-scss-gulp/assets/js/scripts.js
+++ bootstrap-scss-gulp/assets/js/scripts.js
@@ -8,22 +8,69 @@ $(function(){
         'backgroundColor': '#f74902', 'color': 'white'
     });
 
+
+    /// ROBOT INIT ///
+    var robot = 0;
+    console.log('Robot is currently at position: ' + robot);
+
     var output = $('#consoleWindow');
 
 
 
-    var place = $('.place').click(function() {
-        var xy = prompt('Take your robot for a walk! \n Example: \"0,3 NORTH\"');
+    /// POSITION ARRAY ///
+    var position = $('.move').map(function() {
+        return $(this).attr('title');
+    });
+    console.log(position);
 
-        if (xy != null) {
-            output.append('<code> PLACE: ' + xy + '</code><br />');
+
+
+    /// CONSOLE ///
+
+    var consoleWindow = $('.move').click(function() {
+        var title = $(this).attr('title')
+        output.append('<code>' + title + '</code><br />');
+        console.log('hi from robot: ' + robot);
+
+        // switch here?? //
+        if($(this).hasClass('west')) {
+            robot -= 1;
+            console.log('west');
+        } else if($(this).hasClass('south')) {
+            robot -= 1;
+            console.log('south');
+        } else if($(this).hasClass('north')) {
+            robot += 1;
+            console.log('north');
+        } else if($(this).hasClass('east')) {
+            robot += 1;
+            console.log('east');
+        }
+
+
+        if(robot >= 0 && robot <= 4) {
+            console.log('robot is inside the table');
+
+        } else {
+            var warning = prompt('Robot is on a suicide mission! \nAre you sure you want to do this?');
+            if (warning === "yes") {
+                console.log("robot is now outside the table");
+                return true;
+            } else if (warning === "no") {
+                console.log("no");
+                output.text("");
+                robot = 0;
+            } else {
+                output.text("");
+                robot = 0;
+            }
         }
     });
 
 
-    var consoleWindow = $('.move').click(function() {
-        var title = $(this).attr('title')
-        console.log(title);
-        output.append('<code>' + title + '</code><br />');
+    /// REPORT ///
+    $('#reportBtn').click(function(){
+        $('#report').html('<code>' + robot + '</code>')
     });
+
 });
